0a66784f4cfbb660695af306996ee3820f147a2c
Cihan Kaynak <cihan@98point6.com>
2021-12-01 10:43:01 -0600

    Day1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var depthIncreaseCount = 0
+        var previous = Int.MAX_VALUE
+        input.map { it.toInt() }.forEach {
+            if(it > previous ){
+                depthIncreaseCount++
+            }
+            previous = it
+        }
+        return depthIncreaseCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        fun sum(input: List<Int>, start: Int, window: Int): Int{
+            val end = (start + window - 1).coerceAtMost(input.size - 1)
+            return input.slice(start..end).sum()
+        }
+
+        var sumIncreaseCount = 0
+        var previousSum = Int.MAX_VALUE
+        val measurements = input.map { it.toInt() }
+        measurements.forEachIndexed {
+                index, _ ->
+            val sum = sum(measurements, index, 3)
+            if( sum > previousSum ){
+                sumIncreaseCount++
+            }
+            previousSum = sum
+        }
+        return sumIncreaseCount
+    }
 
     val input = readInput("Day01")
     println(part1(input))

4d2ee63e026af53515f10bc6f78daa3b00015a3b
Cihan Kaynak <cihan@98point6.com>
2021-12-03 10:13:52 -0600

    Day-3 Part-2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,12 +1,7 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-
-        // assign points to each index: 1 is 1 0 is -1
-        val points = mutableListOf<Int>()
-
-        var gamma = ""
-        var epsilon = ""
-
+class Points(input: List<String>){
+    // assign points to each index: 1 is 1 0 is -1
+    val points = mutableListOf<Int>()
+    init {
         input.forEach {
             it.forEachIndexed {index, bit ->
                 if( points.size == index ){
@@ -16,6 +11,56 @@ fun main() {
                 points[index] = if( bit == '1') frequency + 1 else frequency - 1
             }
         }
+    }
+}
+
+abstract class LifeSupportRating(private val input: List<String>){
+
+    fun getRating(): String{
+
+        val mutableInput = input.toMutableList()
+        var pointIndex = 0
+        while( mutableInput.size > 1){
+            val points = Points(mutableInput).points
+            val bitToRemove = getBitToRemove(points[pointIndex])
+            mutableInput.removeIf { it[pointIndex] == bitToRemove }
+            pointIndex++
+        }
+        return mutableInput[0]
+    }
+
+    abstract fun getBitToRemove(points: Int): Char
+}
+
+class OxygenRating(input: List<String>): LifeSupportRating(input){
+
+    override fun getBitToRemove(points: Int): Char {
+        return if( points >= 0 ){
+            '0'
+        }else{
+            '1'
+        }
+    }
+}
+
+class CO2Rating(input: List<String>): LifeSupportRating(input){
+
+    override fun getBitToRemove(points: Int): Char {
+        return if( points >= 0 ){
+            '1'
+        }else{
+            '0'
+        }
+    }
+}
+
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        val points = Points(input).points
+        var gamma = ""
+        var epsilon = ""
 
         points.forEach {
             if( it > 0 ){
@@ -31,7 +76,10 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-       return 0
+
+        val oxygenRating = OxygenRating(input)
+        val co2Rating = CO2Rating(input)
+        return oxygenRating.getRating().toInt(2) * co2Rating.getRating().toInt(2)
     }
 
     val input = readInput("Day03")
